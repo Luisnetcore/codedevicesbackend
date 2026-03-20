a19eece4c444dccfed76f2af18ad13bbad69b219
luis alejandro fernandez medina <alejo_a.f@hotmail.com>
2026-03-20 14:56:15 -0500

    punto y coma mal

--- Devices.Application/Utils/DeviceMapperProfile.cs
+++ Devices.Application/Utils/DeviceMapperProfile.cs
@@ -11,7 +11,7 @@ namespace Devices.Application.Utils
             CreateMap<Device, DeviceInformationDTO>();
             CreateMap<Category, CategoryCreateDto>().ReverseMap();
             CreateMap<Category, CategoryInformationDto>().ReverseMap().ForMember(dest => dest.Id,
-                       opt => opt.MapFrom(src => src.CategoryId)); ;
+                       opt => opt.MapFrom(src => src.CategoryId));
 
         }
     }
