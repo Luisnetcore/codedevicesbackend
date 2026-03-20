eac40690e84fcb6bd52c49fe32481ff54b067f77
David GCT <davidgonzalo00@gmail.com>
2026-03-20 15:13:26 -0500

    Se crea las Clases de la clase Brand

---
+++ Devices.Infraestructure/Data/Repository/Repositories/interfaces/IBrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Devices.Domain.Entities;
+using Devices.Infraestructure.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devices.Infraestructure.Data.Repository.Repositories.interfaces
+{
+    public interface IBrandRepository : IRepository<Brand>
+    {
+        
+    }
+}
---
+++ devices.webapi/Controllers/BrandController.cs
@@ -0,0 +1,51 @@
+﻿using Devices.Application.DTOs;
+using Devices.Application.services.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace devices.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+        public BrandController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        // GET: api/<BrandController>
+        [HttpGet]
+        public async Task<List<BrandInformationDTO>> Get()
+        {
+            return await _brandService.GetBrands();
+        }
+
+        // GET api/<BrandController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BrandController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BrandController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BrandController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
